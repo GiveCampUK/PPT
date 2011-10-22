5fd9e7850a0d64578e43a79b1abe9afb1268784b
David Whitney <david@davidwhitney.co.uk>
2011-10-22 09:03:04 +0100

    added lightswitch demo

---
+++ LightswitchTest/Application1/Application1/Client/UserCode/PeopleList.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+using System.IO.IsolatedStorage;
+using System.Collections.Generic;
+using Microsoft.LightSwitch;
+using Microsoft.LightSwitch.Framework.Client;
+using Microsoft.LightSwitch.Presentation;
+using Microsoft.LightSwitch.Presentation.Extensions;
+namespace LightSwitchApplication
+{
+    public partial class PeopleList
+    {
+        partial void Method_Execute()
+        {
+            System.Diagnostics.Debug.WriteLine("boo ya");
+            // Write your code here.
+            
+
+        }
+
+        partial void DoMethod1_Execute()
+        {
+            // Write your code here.
+
+        }
+    }
+}
